51d3f11abb63d1b0a0fc458b22f8286cad093f72
Corvin Deboeser <corvin.deboeser@gmx.de>
2018-11-03 04:45:10 +0100

    Frontend: Added styled Tooltip to librarz

---
+++ client/src/components/common/Tooltip.js
@@ -0,0 +1,11 @@
+import Tooltip from "@material-ui/core/Tooltip";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  tooltip: {
+    fontWeight: 500,
+    fontSize: theme.typography.caption.fontSize
+  }
+});
+
+export default withStyles(styles, { withTheme: true })(Tooltip);
