3afc33a4f5b4db31f36dbede3e331959f8b51763
Corvin Deboeser <corvin.deboeser@gmx.de>
2018-11-03 02:59:01 +0100

    Frontend: Cleanup dependency in Button

--- client/src/components/common/Button.js
+++ client/src/components/common/Button.js
@@ -1,4 +1,4 @@
-import React, { Children } from "react";
+import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import MaterialButton from "@material-ui/core/Button";
 import classnames from "classnames";
